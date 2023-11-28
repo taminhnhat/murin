a0849f44a73a5be51c4637f6ecdb7a3c23227b4b
taminhnhat <taminhnhat98@gmail.com>
2023-11-28 17:21:36 +0700

    i

--- scripts/murin_realsense.sh
+++ scripts/murin_realsense.sh
@@ -4,4 +4,4 @@ date
 export ROS_DOMAIN_ID=3
 source /home/nhattm/dev-ws/install/setup.bash
 printenv | grep ROS
-ros2 run web_video_server web_video_server #> ~/.murin/murin_stream.log
+ros2 launch murin_bringup realsense.launch.py #> ~/.murin/murin_realsense.log
