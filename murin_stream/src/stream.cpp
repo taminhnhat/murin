9f912132daa5e40f1f287d706768cc4c45913c48
taminhnhat <taminhnhat98@gmail.com>
2023-12-29 15:57:36 +0700

    i

--- murin_stream/src/stream.cpp
+++ murin_stream/src/stream.cpp
@@ -22,8 +22,9 @@ public:
     MinimalPublisher() : Node("minimal_publisher"), count_(0)
     {
         publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
+        image_publisher_ = this->create_publisher<sensor_msgs::msg::Image>("front_camera", 10);
 
-        timer_ = this->create_wall_timer(500ms, std::bind(&MinimalPublisher::timer_callback, this));
+        timer_ = this->create_wall_timer(10ms, std::bind(&MinimalPublisher::timer_callback, this));
         cv::namedWindow(OPENCV_WINDOW);
 
         // std::string image_path = "/home/nhattm/dev-ws/src/murin/murin_stream/Lenna.png";
@@ -33,6 +34,14 @@ public:
 
         rmw_qos_profile_t custom_qos = rmw_qos_profile_default;
         subscriber = image_transport::create_subscription(this, "/camera/color/image_raw", std::bind(&MinimalPublisher::imageCallback, this, std::placeholders::_1), "raw", custom_qos);
+
+        int deviceID = 0;
+        int apiID = cv::CAP_ANY;
+        cap.open(deviceID, apiID);
+        if (!cap.isOpened())
+        {
+            std::cerr << "ERROR! Unable to open camera\n";
+        }
     }
 
 private:
@@ -40,20 +49,26 @@ private:
     image_transport::Subscriber subscriber;
     void timer_callback()
     {
-        auto message = std_msgs::msg::String();
-        message.data = "Hello, world! " + std::to_string(count_++);
-        // RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
-        publisher_->publish(message);
+        // auto message = std_msgs::msg::String();
+        // message.data = "Hello, world! " + std::to_string(count_++);
+        // // RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
+        // publisher_->publish(message);
+
+        cv::Mat img;
+        cap >> img;
+        // cv::randu(img, cv::Scalar(0, 0, 0), cv::Scalar(255, 255, 255));
+        sensor_msgs::msg::Image::SharedPtr img_msg = cv_bridge::CvImage(std_msgs::msg::Header(), "bgr8", img).toImageMsg();
+        image_publisher_->publish(*img_msg);
     }
 
-    void imageCallback(const sensor_msgs::msg::Image::ConstSharedPtr &msg)
+    void imageCallback(const sensor_msgs::msg::Image::ConstSharedPtr &img)
     {
         cv_bridge::CvImagePtr cv_ptr;
         try
         {
-            // const std::string encode_ = cv_bridge::getEncoding(msg->encoding);
-            // std::cout << "encoing " << msg->encoding << std::endl;
-            cv_ptr = cv_bridge::toCvCopy(msg, msg->encoding);
+            // const std::string encode_ = cv_bridge::getEncoding(img->encoding);
+            // std::cout << "encoing " << img->encoding << std::endl;
+            cv_ptr = cv_bridge::toCvCopy(img, img->encoding);
         }
         catch (cv_bridge::Exception &e)
         {
@@ -72,6 +87,8 @@ private:
 
     rclcpp::TimerBase::SharedPtr timer_;
     rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+    rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr image_publisher_;
+    cv::VideoCapture cap;
     size_t count_;
 };
 
